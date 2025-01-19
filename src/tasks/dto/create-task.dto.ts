1d4bb0b9d9aad2a7da485755212788e97131221a
Samuel Gomes <samuel.codeapp@gmail.com>
2025-01-19 12:37:45 -0300

    Add dtos para criação e atualização de tasks

---
+++ src/tasks/dto/create-task.dto.ts
@@ -0,0 +1,8 @@
+import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class CreateTaskDto {
+  @IsString()
+  @MinLength(5)
+  @IsNotEmpty()
+  readonly description: string;
+}
