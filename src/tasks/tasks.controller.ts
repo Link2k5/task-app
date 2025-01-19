b7d6a0237993fb8a6564fa176c70945516c41f27
Samuel Gomes <samuel.codeapp@gmail.com>
2025-01-19 13:29:37 -0300

    Criado resource para tasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private readonly taskService: TasksService) {}
+
+  @Get()
+  getTasks() {
+    return this.taskService.getTasks();
+  }
+
+  @Get(':id')
+  getTask(@Param('id', ParseIntPipe) id: number) {
+    return this.taskService.getTask(id);
+  }
+
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto) {
+    return this.taskService.createTask(createTaskDto);
+  }
+
+  @Patch(':id')
+  updateTask(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateTaskDto: UpdateTaskDto,
+  ) {
+    return this.taskService.updateTask(id, updateTaskDto);
+  }
+
+  @Delete(':id')
+  deleteTask(@Param('id', ParseIntPipe) id: number) {
+    return this.taskService.deleteTask(id);
+  }
+}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,79 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getTasks() {
+    return this.prisma.task.findMany();
+  }
+
+  async getTask(id: number) {
+    const task = await this.prisma.task.findFirst({
+      where: {
+        id: id,
+      },
+    });
+
+    if (task?.description) return task;
+
+    throw new HttpException('Task not found', HttpStatus.NOT_FOUND);
+  }
+
+  async createTask(createTaskDto: CreateTaskDto) {
+    const newTask = this.prisma.task.create({
+      data: {
+        description: createTaskDto.description,
+        completed: false,
+      },
+    });
+
+    return newTask;
+  }
+
+  async updateTask(id: number, updateTaskDto: UpdateTaskDto) {
+    const findTask = await this.prisma.task.findFirst({
+      where: {
+        id: id,
+      },
+    });
+
+    if (!findTask) {
+      throw new HttpException('Task not found', HttpStatus.NOT_FOUND);
+    }
+
+    const task = this.prisma.task.update({
+      where: {
+        id: findTask.id,
+      },
+      data: updateTaskDto,
+    });
+
+    return task;
+  }
+
+  async deleteTask(id: number) {
+    const findTask = await this.prisma.task.findFirst({
+      where: {
+        id: id,
+      },
+    });
+
+    if (!findTask) {
+      throw new HttpException('Task not found', HttpStatus.NOT_FOUND);
+    }
+
+    await this.prisma.task.delete({
+      where: {
+        id: findTask.id,
+      },
+    });
+
+    return {
+      message: 'Task Deleted',
+    };
+  }
+}
